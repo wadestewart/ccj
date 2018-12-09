5d4715cf1a7404bc73b46e4fe86e081c5080484a
Wade Stewart <stew.wade@gmail.com>
2018-12-09 12:39:42 -0500

    deleted svg, could not render. added jquery dropdown to leadership element

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ $('.nav-scroll').on('click', function(e) {
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 1000)
 })
+
+$('#dropdown').hover(
+    function() {
+        $(this).find('ul').slideDown()
+    },
+    function() {
+        $(this).find('ul').slideUp()
+    }
+)
