e0aa191eca4e52f191e568dee18c5208bd080ef8
Wade Stewart <stew.wade@gmail.com>
2018-12-08 21:09:09 -0500

    added script for on click to navbar elements, added dropdown for leadership element

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+$('.nav-scroll').on('click', function(e) {
+    e.preventDefault()
+    $('body, html').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 1000)
+})
